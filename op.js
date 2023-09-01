2031d11f3e093c2b8bf657beb413c71a340db3be
Marcus Brazão <141243829+SargentoPimenta@users.noreply.github.com>
2023-09-01 11:43:53 -0300

    Add files via upload

---
+++ op.js
@@ -0,0 +1,12 @@
+function soma (){
+    return (soma = a + b)
+}
+function mult (){
+    return (soma = a + b)
+}
+function sub (){
+    return (soma = a + b)
+}
+function div (){
+    return (soma = a + b)
+}
---
+++ script.mjs
@@ -0,0 +1,3 @@
+import * as palmolengo from "./operacoes.mjs"
+console.log(palmolengo.hello())
+console.log(palmolengo.gd())
